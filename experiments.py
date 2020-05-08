7520351201eb315549959ab672fd5cc489ea9c03
shizhec <shizhec@shizhec-ubuntu18.04>
2020-05-09 01:37:53 +1000

    half way

--- experiments.py
+++ experiments.py
@@ -1,34 +1,83 @@
+import os
 import gym
-from stable_baselines.common.policies import MlpPolicy, CnnPolicy, LstmPolicy, BasePolicy, FeedForwardPolicy, ActorCriticPolicy
 from stable_baselines import A2C
 from stable_baselines.common import make_vec_env
+from stable_baselines.common.cmd_util import make_atari_env
+from stable_baselines.common.vec_env import VecFrameStack
+from stable_baselines import results_plotter
+from stable_baselines.results_plotter import load_results
+from stable_baselines.bench import Monitor
+import numpy as np
+from matplotlib import pyplot as plt
 
+def get_para_dict(gamma, n_step, learning_rate, alpha, epsilon):
+    para_dict = {}
 
-def run_experiment(game_name, parameter_dict, render=False):
-    reward_list = []
+    para_dict["gamma"] = gamma
+    para_dict["n_step"] = n_step
+    para_dict["learning_rate"] = learning_rate
+    para_dict["alpha"] = alpha
+    para_dict["epsilon"] = epsilon
 
-    # parallel environment
-    env = make_vec_env(game_name, n_envs=parameter_dict["n_envs"])
+    return para_dict
+
+def run_episode_mean_reward_experiment(game_name, policy, parameter_dict, timesteps=25000, render=False, atari_game=False):
+
+    # # make parallel environment
+    # env = make_vec_env(game_name, n_envs=4)
+    
+    # make environment
+    if not atari_game:
+        env = gym.make(game_name)
+    else:
+        env = make_atari_env(game_name, num_env=1, seed=0)
+        env = VecFrameStack(env, n_stack=4)
 
     # model defination
-    model = A2C(parameter_dict["policy"], env, verbose=1)
-    model.load_parameters(parameter_dict)
+    model = A2C(policy, env, verbose=1, gamma=parameter_dict["gamma"]
+                                      , n_steps=parameter_dict["n_step"]
+                                      , alpha=parameter_dict["alpha"]
+                                      , learning_rate=parameter_dict["learning_rate"]
+                                      , epsilon=parameter_dict["epsilon"])
 
-    model.learn(total_timesteps=parameter_dict["total_timesteps"])
-    model.save("a2c_"+ game_name)
+    episode_rewards = []
+    for i in range(timesteps):
+        reward_sum = 0
+        done = False
+        obs = env.reset()
+        while not done:
+            action, _states = model.predict(obs)
+            obs, reward, done, info = env.step(action)
+            reward_sum += reward
+        episode_rewards.append(reward_sum)
+        if i % 100==0:
+            print("finished episode " + str(i))
+            print(np.mean(episode_rewards))
     
-    del model
-
-    model.load("a2c_"+ game_name)
-    obs = env.reset()
-    while True:
-        action, _states = model.predict(obs)
-        obs, rewards, dones, info = env.step(action)
-        if render==True:
-            env.render(mode='rgb_array')
-        reward_list.append(rewards)
+    return np.mean(episode_rewards)
             
+def run_reward_curve_experiments(game_name, policy, parameter_dict, timesteps=25000, atari_game=False):
+    log_dir = "/tmp/"
 
+    # make environment
+    if not atari_game:
+        env = gym.make(game_name)
+    else:
+        env = make_atari_env(game_name, num_env=1, seed=0)
+        env = VecFrameStack(env, n_stack=4)
+    
+    env = Monitor(env, log_dir)
+
+    # model defination
+    model = A2C(policy, env, verbose=1, gamma=parameter_dict["gamma"]
+                                      , n_steps=parameter_dict["n_step"]
+                                      , alpha=parameter_dict["alpha"]
+                                      , learning_rate=parameter_dict["learning_rate"]
+                                      , epsilon=parameter_dict["epsilon"])
+    model.learn(timesteps)
+
+    results_plotter.plot_results([log_dir], timesteps, results_plotter.X_TIMESTEPS, "A2C "+game_name)
+    plt.show()
 
 
 
--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import experiments
+from stable_baselines.common.policies import MlpPolicy, CnnPolicy, LstmPolicy, BasePolicy, FeedForwardPolicy, ActorCriticPolicy
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
-    print("test1")
+    
+    para_dict = experiments.get_para_dict(gamma=0.99, n_step=5, learning_rate=0.0007, alpha=0.99, epsilon=1e-05)
+    # episode_mean_rewards = experiments.run_episode_mean_reward_experiment("MountainCarContinuous-v0", MlpPolicy, para_dict, timesteps=25000)
+    # print(episode_mean_rewards)
+
+
+
+    experiments.run_reward_curve_experiments("Acrobot-v1", MlpPolicy, para_dict, timesteps=25000)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+# 22.16092
+# 22.20868
