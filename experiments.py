58f8f644974713dc0f68c7921a5eff08aa1f1dc1
shizhec <shizhec@shizhec-ubuntu18.04>
2020-05-11 01:27:05 +1000

    finish a2c learning rate tuning

--- experiments.py
+++ experiments.py
@@ -1,16 +1,18 @@
 import os
 import gym
 from stable_baselines import A2C
+from stable_baselines import DDPG
 from stable_baselines.common import make_vec_env
 from stable_baselines.common.cmd_util import make_atari_env
 from stable_baselines.common.vec_env import VecFrameStack
 from stable_baselines import results_plotter
 from stable_baselines.results_plotter import load_results
 from stable_baselines.bench import Monitor
+from stable_baselines.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise, AdaptiveParamNoiseSpec
 import numpy as np
 from matplotlib import pyplot as plt
 
-def get_para_dict(gamma, n_step, learning_rate, alpha, epsilon):
+def get_a2c_para_dict(gamma, n_step, learning_rate, alpha, epsilon):
     para_dict = {}
 
     para_dict["gamma"] = gamma
@@ -21,14 +23,27 @@ def get_para_dict(gamma, n_step, learning_rate, alpha, epsilon):
 
     return para_dict
 
-def run_episode_mean_reward_experiment(game_name, policy, parameter_dict, timesteps=25000, render=False, atari_game=False):
+def get_ddpg_para_dict(gamma, nb_train_steps, nb_rollout_steps, nb_eval_steps, 
+                       batch_size, actor_lr, critic_lr, buffer_size, reward_scale):
+    para_dict = {}
 
-    # # make parallel environment
-    # env = make_vec_env(game_name, n_envs=4)
+    para_dict["gamma"] = gamma
+    para_dict["nb_train_steps"] = nb_train_steps
+    para_dict["nb_rollout_steps"] = nb_rollout_steps
+    para_dict["nb_eval_steps"] = nb_eval_steps
+    para_dict["batch_size"] = batch_size
+    para_dict["actor_lr"] = actor_lr
+    para_dict["critic_lr"] = critic_lr
+    para_dict["buffer_size"] = buffer_size
+    para_dict["reward_scale"] = reward_scale
+
+    return para_dict
+
+def run_episode_mean_reward_experiment_a2c(game_name, solved_score, policy, parameter_dict, episode=500, timesteps=25000, render=False, atari_game=False):
     
     # make environment
     if not atari_game:
-        env = gym.make(game_name)
+        env = make_vec_env(game_name)
     else:
         env = make_atari_env(game_name, num_env=1, seed=0)
         env = VecFrameStack(env, n_stack=4)
@@ -41,27 +56,46 @@ def run_episode_mean_reward_experiment(game_name, policy, parameter_dict, timest
                                       , epsilon=parameter_dict["epsilon"])
 
     episode_rewards = []
-    for i in range(timesteps):
+    total_timesteps = 0
+    for i in range(episode):
         reward_sum = 0
         done = False
         obs = env.reset()
         while not done:
             action, _states = model.predict(obs)
             obs, reward, done, info = env.step(action)
+
+            total_timesteps += 1
+            if total_timesteps > timesteps:
+                break
+
             reward_sum += reward
+            if render == True:
+                env.render()
         episode_rewards.append(reward_sum)
-        if i % 100==0:
-            print("finished episode " + str(i))
-            print(np.mean(episode_rewards))
-    
-    return np.mean(episode_rewards)
+
+        if i % 100 == 0:
+            print("episode: "+str(i))
+        # print('\rEpisode {}, Episode Score: {}, Max: {:.2f}, Min: {:.2f}, Steps: {}'\
+        #           .format(i, reward_sum, np.max(episode_rewards), np.min(episode_rewards), total_timesteps), end="\n")
+
+        if reward_sum >= solved_score:
+            break
+    # print('\rAverage Rewards {}'.format(np.mean(episode_rewards)))
+    # plt.plot(np.arange(1, len(episode_rewards)+1), episode_rewards)
+    # plt.ylabel('episode reward')
+    # plt.xlabel('# of episode')
+    # plt.title('a2c '+game_name)
+    # plt.show()
+    return np.max(episode_rewards)
+
             
-def run_reward_curve_experiments(game_name, policy, parameter_dict, timesteps=25000, atari_game=False):
+def run_experiments_moniter_a2c(game_name, policy, parameter_dict, timesteps=25000, atari_game=False):
     log_dir = "/tmp/"
 
     # make environment
     if not atari_game:
-        env = gym.make(game_name)
+        env = make_vec_env(game_name)
     else:
         env = make_atari_env(game_name, num_env=1, seed=0)
         env = VecFrameStack(env, n_stack=4)
@@ -80,6 +114,58 @@ def run_reward_curve_experiments(game_name, policy, parameter_dict, timesteps=25
     plt.show()
 
 
+def run_episode_mean_reward_experiment_ddpg(game_name, solved_score, policy, parameter_dict, episode=500 ,timesteps=25000, render=False, atari_game=False):
 
+    # make environment
+    if not atari_game:
+        env = make_vec_env(game_name)
+    else:
+        env = make_atari_env(game_name, num_env=1, seed=0)
+        env = VecFrameStack(env, n_stack=4)
 
+    n_actions = env.action_space.shape[-1]
+    action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=float(0.5) * np.ones(n_actions))
+
+    # model defination
+    model = DDPG(policy, env, action_noise=action_noise ,verbose=1, gamma=parameter_dict["gamma"]
+                                        , nb_train_steps=parameter_dict["nb_train_steps"]
+                                        , nb_rollout_steps=parameter_dict["nb_rollout_steps"]
+                                        , nb_eval_steps=parameter_dict["nb_eval_steps"]
+                                        , batch_size=parameter_dict["batch_size"]
+                                        , actor_lr=parameter_dict["actor_lr"]
+                                        , critic_lr=parameter_dict["critic_lr"]
+                                        , buffer_size=parameter_dict["buffer_size"]
+                                        , reward_scale=parameter_dict["reward_scale"])
+
+    episode_rewards = []
+    total_timesteps = 0
+    for i in range(episode):
+        reward_sum = 0
+        done = False
+        obs = env.reset()
+        while not done:
+            action, _states = model.predict(obs)
+            obs, reward, done, info = env.step(action)
+            total_timesteps += 1
+
+            if total_timesteps > timesteps:
+                break
+
+            if render == True:
+                env.render()
+
+            reward_sum += reward
+        episode_rewards.append(reward_sum)
+
+        print('\rEpisode {}, Episode Score: {}, Max: {:.2f}, Min: {:.2f}, total_steps: {:.2f}'\
+                  .format(i, reward_sum, np.max(episode_rewards), np.min(episode_rewards), total_timesteps), end="\n")
+
+        if reward_sum >= solved_score:
+            break
+    print('\rAverage Rewards {}'.format(np.mean(episode_rewards)))
+    plt.plot(np.arange(1, len(episode_rewards)+1), episode_rewards)
+    plt.ylabel('episode reward')
+    plt.xlabel('# of episode')
+    plt.title('ddpg '+game_name)
+    plt.show()
  
--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 import experiments
-from stable_baselines.common.policies import MlpPolicy, CnnPolicy, LstmPolicy, BasePolicy, FeedForwardPolicy, ActorCriticPolicy
+from stable_baselines.common.policies import MlpPolicy, CnnPolicy, LstmPolicy, MlpLstmPolicy, CnnLstmPolicy
+from stable_baselines.ddpg.policies import MlpPolicy as ddpg_mlpPolicy
+from loguniform import LogUniform
 import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
     
-    para_dict = experiments.get_para_dict(gamma=0.99, n_step=5, learning_rate=0.0007, alpha=0.99, epsilon=1e-05)
-    # episode_mean_rewards = experiments.run_episode_mean_reward_experiment("MountainCarContinuous-v0", MlpPolicy, para_dict, timesteps=25000)
-    # print(episode_mean_rewards)
+    # a2c_para_dict = experiments.get_a2c_para_dict(gamma=0.99, n_step=5, learning_rate=1e-06 , alpha=0.99, epsilon=1e-05)
+    # run the a2c problem
+    # experiments.run_episode_mean_reward_experiment_a2c('CartPole-v1', 195.0, MlpPolicy, a2c_para_dict, episode=10000, timesteps=1e7, render=False, atari_game=False)
+    # experiments.run_experiments_moniter_a2c('CartPole-v1', MlpPolicy, a2c_para_dict)
+
+    # ddpg_para_dict = experiments.get_ddpg_para_dict(gamma=0.99, nb_train_steps=50, nb_rollout_steps=100, 
+    #                                                 nb_eval_steps=100, batch_size=128, actor_lr=0.0001, critic_lr=0.001, buffer_size=50000, reward_scale=1.0)
+    # experiments.run_episode_mean_reward_experiment_ddpg('LunarLanderContinuous-v2', 200.0, ddpg_mlpPolicy, ddpg_para_dict, episode=5000, timesteps=1e7, render=False, atari_game=False)
+    run_a2c_learning_rate_exp('CartPole-v1', MlpPolicy)
 
 
 
-    experiments.run_reward_curve_experiments("Acrobot-v1", MlpPolicy, para_dict, timesteps=25000)
 
+def run_a2c_learning_rate_exp(game, policy):
+    max_reward_list = []
+    # d1 = LogUniform(0.000001, 0.01)
+    # learning_rate_list = d1.rvs(100)
+    learning_rate_list = np.arange(0.000001, 0.01, 0.00008)
 
+    i = 1
+    for learning_rate in learning_rate_list:
+        print('\rrunning experiment {} with learning_rate {}'.format(i, learning_rate))
+        a2c_para_dict = experiments.get_a2c_para_dict(gamma=0.99, n_step=5, learning_rate=learning_rate , alpha=0.99, epsilon=1e-05)
+        max_reward = experiments.run_episode_mean_reward_experiment_a2c(game, 195.0, policy, a2c_para_dict, episode=5000, timesteps=1e7, render=False, atari_game=False)
+        max_reward_list.append(max_reward)
 
-
-
-
-
+        i += 1
+    
+    
+    plt.plot(learning_rate_list, max_reward_list, 'ro')
+    plt.xlabel('learning_rate')
+    plt.ylabel('max_reward')
+    plt.show()
 
 if __name__ == "__main__":
     main()
-
-# 22.16092
-# 22.20868
